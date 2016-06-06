a7c5c27339bb8e725ae34f493fce42e6eae06e9f
Mohamed Alsadek <mohamedalsadek@VISs-MacBook-Pro.local>
2016-06-06 15:02:36 +0200

    configuring base architecture of the project

---
+++ GE_Test/LocationService.m
@@ -0,0 +1,13 @@
+//
+//  LocationService.m
+//  GE_Test
+//
+//  Created by Mohamed Alsadek on 6/6/16.
+//  Copyright © 2016 Mohamed Alsadek. All rights reserved.
+//
+
+#import "LocationService.h"
+
+@implementation LocationService
+
+@end
