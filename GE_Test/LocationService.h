c6197ab7bfe0822724543f1c87c8c97b6fae5b39
Mohamed Alsadek <mohamedalsadek@VISs-MacBook-Pro.local>
2016-06-06 15:13:48 +0200

    adding User LocationService

--- GE_Test/LocationService.h
+++ GE_Test/LocationService.h
@@ -7,7 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import <UIKit/UIKit.h>
 
 @interface LocationService : NSObject
+
+
+- (void)startUpdatingLocation;
+- (void)stopUpdatingLocation;
+
+- (BOOL)isLocationServicesEnabled;
+- (BOOL)hasLocationAccessPermission;
+
+@property (copy, nonatomic) void (^updateLocation)(CLLocationCoordinate2D location, NSString *errorMSG);
 
 @end
--- GE_Test/LocationService.m
+++ GE_Test/LocationService.m
@@ -8,6 +8,94 @@
 
 #import "LocationService.h"
 
+
+@interface LocationService ()<CLLocationManagerDelegate>
+
+@property (nonatomic, strong) CLLocationManager *gps;
+
+@end
+
+
 @implementation LocationService
+
+- (void)startUpdatingLocation   {
+    
+    if (!self.gps) {
+        self.gps = [[CLLocationManager alloc] init];
+        self.gps.delegate = self;
+        [self.gps setDesiredAccuracy:kCLLocationAccuracyBest];
+        
+        if (![self isLocationServicesEnabled]) {
+            
+            if (self.updateLocation) {
+                self.updateLocation(CLLocationCoordinate2DMake(0, 0), @"Can't find user location, please enable location service");
+            }
+            
+            return;
+        }
+        switch ([CLLocationManager authorizationStatus]) {
+            case kCLAuthorizationStatusDenied:
+            case kCLAuthorizationStatusRestricted:
+            {
+                // Error AuthorizationStatusRestricted
+                
+                if (self.updateLocation) {
+                    self.updateLocation(CLLocationCoordinate2DMake(0, 0), @"Can't get userlocation, please enable location service");
+                    self.updateLocation = nil;
+                }
+                return;
+            }
+                break;
+            case kCLAuthorizationStatusNotDetermined:
+                if([self.gps respondsToSelector:@selector(requestWhenInUseAuthorization)])
+                    [self.gps requestWhenInUseAuthorization];
+                break;
+            default:
+                break;
+        }
+    }
+    
+    [self.gps startUpdatingLocation];
+}
+
+
+- (BOOL)isLocationServicesEnabled
+{
+    return [CLLocationManager locationServicesEnabled];
+}
+- (BOOL)hasLocationAccessPermission
+{
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        return YES;
+    }
+    return NO;
+}
+
+- (void)stopUpdatingLocation    {
+    
+    [self.gps stopUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error  {
+    // Error get location
+    
+    [self stopUpdatingLocation];
+    __weak __typeof(self)weakSelf = self;
+    if (weakSelf.updateLocation) {
+        weakSelf.updateLocation(CLLocationCoordinate2DMake(0, 0), error.localizedDescription);
+        weakSelf.updateLocation = nil;
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation   {
+    NSLog(@"didUpdateToLocation: %@", newLocation);
+    
+    __weak __typeof(self)weakSelf = self;
+    if (weakSelf.updateLocation) {
+        [weakSelf stopUpdatingLocation];
+        weakSelf.updateLocation(newLocation.coordinate, nil);
+        weakSelf.updateLocation = nil;
+    }
+}
 
 @end
